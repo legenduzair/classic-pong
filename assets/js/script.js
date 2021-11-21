75f67e4b9a01d635b5f20c5a0fbfb8372b67c340
legenduzair <uzairoomer@live.com>
2021-11-21 16:37:44 +0000

    Edit function name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext("2d");
 
-function render() {
+function drawBoard() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-render();
+drawBoard();
