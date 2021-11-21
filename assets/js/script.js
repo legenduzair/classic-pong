e34909d3e9b2ca28314d2cb6c04edf0a8533c3b2
legenduzair <uzairoomer@live.com>
2021-11-21 17:29:32 +0000

    Add variable for net and draw net onto canvas in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext("2d");
 
+const net = {
+    x: canvas.width/2 - 6/2,
+    y: 0,
+    width: 6,
+    height: canvas.width,
+    color: 'white'
+};
+
+function drawNet() {
+    ctx.fillStyle = net.color
+    ctx.fillRect(net.x, net.y, net.width, net.height)
+}
+
 function drawBoard() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-drawBoard();
+function render () {
+    drawBoard();
+    drawNet();
+}
+
+render();
