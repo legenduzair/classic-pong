d6518a1b3d6d13d3ec12ef9369cf6cf120a0849f
legenduzair <uzairoomer@live.com>
2021-11-22 12:50:02 +0000

    Add user paddle to canvas by adding function and variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext("2d");
 
+/* Game Objects */
+
 const net = {
     x: canvas.width/2 - 6/2,
     y: 0,
@@ -9,19 +11,33 @@ const net = {
     color: 'white'
 };
 
-function drawNet() {
-    ctx.fillStyle = net.color
-    ctx.fillRect(net.x, net.y, net.width, net.height)
-}
+const user = {
+    x: 10,
+    y: canvas.height/2 - 150/2,
+    width: 20,
+    height: 150,
+    color: 'white'
+};
 
 function drawBoard() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+function drawNet() {
+    ctx.fillStyle = net.color;
+    ctx.fillRect(net.x, net.y, net.width, net.height)
+}
+
+function drawUserPaddle() {
+    ctx.fillStyle = user.color;
+    ctx.fillRect(user.x, user.y, user.width, user.height);
+}
+
 function render () {
     drawBoard();
     drawNet();
+    drawUserPaddle();
 }
 
 render();
